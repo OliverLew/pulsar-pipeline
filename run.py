bdffdb6a6d0b0620f72299635f9a9ebc1ebb0ed5
Oliver Lew <oliver_lew@outlook.com>
2019-12-09 19:18:22 +0800

    ignore output, change some loggings

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import sys
 import csv
 import logging
-from subprocess import run, PIPE
+from subprocess import run
 
 logging.basicConfig(level=logging.INFO, format="%(filename)s: %(message)s")
 
@@ -14,16 +14,16 @@ if __name__ == '__main__':
         logging.error("Pass the fortran executable as the argument")
         exit(1)
     if not os.path.exists("filterdata.csv"):
-        logging.error("\"filterdata.csv\" does not exist, run fetchdata.py first")
+        logging.error("\"filterdata.csv\" does not exist, run fetchdata.py")
         exit(1)
     with open("filterdata.csv") as f:
         count = 0
         reader = csv.DictReader(f)
         for data in reader:
             count = count + 1
-            input = "{} {} {} {}".format(data['JName'], data['Age'],
-                                           data['Dist'], data['Edot'])
-            p = run([sys.argv[1]], stdout=PIPE, input=input, encoding='ascii')
+            inputdata = "{} {} {} {}".format(data['JName'], data['Age'],
+                                             data['Dist'], data['Edot'])
             logging.info("Running source {}".format(count))
-            print(p.stdout)
+            logging.info(inputdata)
+            p = run([sys.argv[1]], input=inputdata, encoding='ascii')
             print()
