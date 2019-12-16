4b87e932b41277ade218a4ce5fdb5cf79438e4a0
Oliver Lew <oliver_lew@outlook.com>
2019-12-16 22:31:29 +0800

    split functions

--- plot.py
+++ plot.py
@@ -7,6 +7,28 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt  # noqa
 
 
+def ploteach(source):
+    name = source['JName']
+    plt.figure(figsize=(8, 6))
+    plt.title('Source: {}'.format(name))
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('$E$')
+    plt.ylabel('$E^{3}flux$')
+    for txt in os.listdir(outputdir):
+        if txt.startswith(name):
+            alpha = float(txt[len(name) + 1: -4])
+            # filter data below floating point precision
+            E, flux = np.loadtxt(os.path.join(outputdir, txt)).T
+            E3flux = flux * E**3
+            plt.plot(E[flux > 1e-308], E3flux[flux > 1e-308],
+                     label='$\\alpha={}$'.format(alpha))
+    plt.legend(framealpha=1)
+    plt.savefig(os.path.join(plotdir, "{}.png".format(name)))
+    plt.savefig(os.path.join(plotdir, "{}.eps".format(name)))
+    plt.close()
+
+
 if __name__ == '__main__':
     resultdir = "result"
     outputdir = os.path.join(resultdir, "output")
@@ -16,22 +38,4 @@ if __name__ == '__main__':
 
     with open("filterdata.csv") as f:
         for source in csv.DictReader(f):
-            name = source['JName']
-            plt.figure(figsize=(8, 6))
-            plt.title('Source: {}'.format(name))
-            plt.xscale('log')
-            plt.yscale('log')
-            plt.xlabel('$E$')
-            plt.ylabel('$E^{3}flux$')
-            for txt in os.listdir(outputdir):
-                if txt.startswith(name):
-                    alpha = float(txt[len(name) + 1: -4])
-                    # filter data below floating point precision
-                    E, flux = np.loadtxt(os.path.join(outputdir, txt)).T
-                    E3flux = flux * E**3
-                    plt.plot(E[flux > 1e-308], E3flux[flux > 1e-308],
-                             label='$\\alpha={}$'.format(alpha))
-            plt.legend(framealpha=1)
-            plt.savefig(os.path.join(plotdir, "{}.png".format(name)))
-            plt.savefig(os.path.join(plotdir, "{}.eps".format(name)))
-            plt.close()
+            ploteach(source)
